780401ec446de2e0ea272555b20b7c9f0108aa00
Griffith0110 <118409402+Griffith0110@users.noreply.github.com>
2026-07-22 00:46:01 +0200

    Improve geolocation input and API client initialization
    Refactor geolocation handling and Open-Meteo client setup.

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import requests_cache
 from retry_requests import retry
 import streamlit as st
 
+if user_input:
+    geolocator = geopy.geocoders.Nominatim(user_agent="ET0")
+try:
+    location = geolocator.geocode(st.chat_input("Position"))
+except Exception as e:
+    location = None
+    st.error(f"Errore durante la ricerca della posizione: {e}")
+
+
 # Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-openmeteo = openmeteo_requests.Client(session=retry_session)
-
-geolocator = geopy.geocoders.Nominatim(user_agent="ET0")
-location = geolocator.geocode(st.text_input("Position"),value="Genga")
-
+if location:
+    cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
 
 # Make sure all required weather variables are listed here
 # The order of variables in hourly or daily is important to assign them correctly below
