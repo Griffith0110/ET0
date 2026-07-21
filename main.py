cd76fbb5713573c9c6279e5dcc7736dd2b15d1bb
Griffith0110 <118409402+Griffith0110@users.noreply.github.com>
2026-07-22 00:35:55 +0200

    Update header and badge formatting in main.py

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ responses = openmeteo.weather_api(url, params=params)
 
 # Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
-st.header("Daily ET0")
-st.subheader(f"📍 {location.address}",text_alignment="justify")
-st.badge(f"🧭 {response.Latitude()}°N  {response.Longitude()}°E", color="green")
+st.subheader("Daily ET0")
+st.write(f"📍 {location.address}",text_alignment="justify")
+st.badge(f"🧭 {response.Latitude()}°N\t{response.Longitude()}°E", color="green")
 st.badge(f"⛰️ {response.Elevation()} m slm", color="yellow")
 
 # Process hourly data. The order of variables needs to be the same as requested.
