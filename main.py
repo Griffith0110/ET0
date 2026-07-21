00cd8d7b33a0ab8e080321e3b58d31f25ace407d
Griffith0110 <118409402+Griffith0110@users.noreply.github.com>
2026-07-22 00:16:15 +0200

    Update daily data processing for precipitation sum

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ url = "https://api.open-meteo.com/v1/forecast"
 params = {
     f"latitude": {location.latitude},
     f"longitude": {location.longitude},
-    "hourly": [
-        "rain_sum",
-        "et0_fao_evapotranspiration"
+    "daily": [
+        "et0_fao_evapotranspiration",
+        "precipitation_sum",
     ],
     "models":"italia_meteo_arpae_icon_2i",
     "timezone": "auto",
@@ -39,8 +39,8 @@ st.badge(f"Elevation {response.Elevation()} m asl", color="yellow")
 
 # Process hourly data. The order of variables needs to be the same as requested.
 daily = response.Daily()
+daily_precipitation_sum = daily.Variables(0).ValuesAsNumpy()
 daily_et0_fao_evapotranspiration = daily.Variables(1).ValuesAsNumpy()
-daily_precipitation = daily.Variables(0).ValuesAsNumpy()
 
 daily_data = {
     "date": pd.date_range(
@@ -51,7 +51,7 @@ daily_data = {
     ).tz_convert(response.Timezone().decode())
 }
 daily_data["ET0 (FAO) mm/m² "] = daily_et0_fao_evapotranspiration
-daily_data["Precipitation mm/m² "] = daily_precipitation
+daily_data["Precipitation mm/m² "] = daily_precipitation_sum
 daily_dataframe = pd.DataFrame(data=daily_data)
 st.dataframe(daily_dataframe)
 st.badge("Data provided by https://open-meteo.com - © 2022–2026 Open-Meteo (CC BY 4.0)")
